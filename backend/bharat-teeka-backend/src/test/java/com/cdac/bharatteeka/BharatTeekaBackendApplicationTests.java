4da7c6914df8ae94ad98d51e570748e84805e2ca
Omkar Gaikwad <omkargaikwad2810@gmail.com>
2026-01-19 14:18:46 +0530

    project base structure with frontend and backend created

---
+++ backend/bharat-teeka-backend/src/test/java/com/cdac/bharatteeka/BharatTeekaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdac.bharatteeka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BharatTeekaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
